66b961f028960798976a7fc1af78567b272f69a7
Michael Pabst <michael.pabst@cs.stanford.edu>
2021-07-22 13:27:31 -0500

    Engine class

--- index.js
+++ index.js
@@ -6,93 +6,99 @@ import {
   styleModule
 } from 'https://unpkg.com/snabbdom@3.0.3?module'
 
-let session
+class Engine {
+  patch = initSnabbdom([propsModule, styleModule, eventListenersModule])
+  container = document.getElementById('container')
 
-const patch = initSnabbdom([propsModule, styleModule, eventListenersModule])
-const container = document.getElementById('container')
-let vDOM
+  session
+  vDOM
 
-async function init() {
-  let resp = await fetch('index.pl')
-  if (!resp.ok) console.error(resp)
-  let src = await resp.text()
+  async init() {
+    let resp = await fetch('index.pl')
+    if (!resp.ok) console.error(resp)
+    let src = await resp.text()
 
-  session = pl.create()
-  await consult(src)
-  await tick()
-}
+    this.session = pl.create()
+    await this.consult(src)
+    await this.tick()
+  }
 
-async function answer() {
-  return new Promise((resolve, reject) => {
-    session.answer({
-      success: resolve,
-      error: reject,
-      fail: reject,
-      limit: reject
+  async answer() {
+    return new Promise((resolve, reject) => {
+      this.session.answer({
+        success: resolve,
+        error: reject,
+        fail: reject,
+        limit: reject
+      })
     })
-  })
-}
-
-async function consult(src) {
-  return new Promise((success, error) => {
-    session.consult(src, { success, error })
-  })
-}
-
-async function handleEvent(ev) {
-  await query(`write_event('${ev.target.id}', '${ev.type}').`)
-  await answer()
-  await tick()
-}
-
-async function query(q) {
-  return new Promise((success, error) => session.query(q, { success, error }))
-}
-
-function term2props(term) {
-  const on = {}
-  const style = {}
-  const props = {}
-  for (const [k, v] of prolog2h(term))
-    if (k === 'on') on[v] = handleEvent
-    else props[k] = v
-  return { on, style, props }
-}
-
-function prolog2h(pl) {
-  if (!pl.indicator) return [pl.value]
-
-  // else we're a Term
-  const { id, indicator, args } = pl
-
-  switch (indicator) {
-    case 'h/2':
-      return [h(args[0].id, term2props(args[1]), [])]
-    case 'h/3':
-      return [h(args[0].id, term2props(args[1]), prolog2h(args[2]))]
-    case './2':
-      return prolog2h(args[0]).concat(prolog2h(args[1]))
-    case '[]/0':
-      return []
   }
 
-  switch (indicator.match(/\/(\d+)$/)[1]) {
-    case '0':
-      return [id]
-    case '1':
-      // props
-      return [[id, args[0].id]]
+  async consult(src) {
+    return new Promise((success, error) => {
+      this.session.consult(src, { success, error })
+    })
   }
 
-  throw `unknown: ${indicator}`
+  async handleEvent(ev) {
+    await this.query(`write_event('${ev.target.id}', '${ev.type}').`)
+    await this.answer()
+    await this.tick()
+  }
+
+  handleEventBound = this.handleEvent.bind(this)
+
+  prolog2h(pl) {
+    if (!pl.indicator) return [pl.value]
+
+    // else we're a Term
+    const { id, indicator, args } = pl
+
+    switch (indicator) {
+      case 'h/2':
+        return [h(args[0].id, this.term2props(args[1]), [])]
+      case 'h/3':
+        return [h(args[0].id, this.term2props(args[1]), this.prolog2h(args[2]))]
+      case './2':
+        return this.prolog2h(args[0]).concat(this.prolog2h(args[1]))
+      case '[]/0':
+        return []
+    }
+
+    switch (indicator.match(/\/(\d+)$/)[1]) {
+      case '0':
+        return [id]
+      case '1':
+        // props
+        return [[id, args[0].id]]
+    }
+
+    throw `unknown: ${indicator}`
+  }
+
+  async query(q) {
+    return new Promise((success, error) =>
+      this.session.query(q, { success, error })
+    )
+  }
+
+  term2props(term) {
+    const on = {}
+    const style = {}
+    const props = {}
+    for (const [k, v] of this.prolog2h(term))
+      if (k === 'on') on[v] = this.handleEventBound
+      else props[k] = v
+    return { on, style, props }
+  }
+
+  async tick() {
+    await this.query('process_tick(Html).')
+    const rendering = await this.answer()
+    const newVDOM = this.prolog2h(rendering.links.Html)[0]
+    this.patch(this.vDOM || this.container, newVDOM)
+    this.vDOM = newVDOM
+  }
 }
 
-async function tick() {
-  await query('process_tick(Html).')
-  const rendering = await answer()
-  const newVDOM = prolog2h(rendering.links.Html)[0]
-  patch(vDOM || container, newVDOM)
-  vDOM = newVDOM
-}
-
-init()
+new Engine().init()
