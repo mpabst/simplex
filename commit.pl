46e7ac2603c9d26cdf06c121f351c382e25b517c
Michael Pabst <michael.pabst@cs.stanford.edu>
2021-08-15 16:53:19 -0500

    Refactoring commit.pl

--- commit.pl
+++ commit.pl
@@ -1,69 +1,53 @@
 :- use_module(library(lists)).
 
-collect_graphs(Patch, Graphs) :-
-  collect_graphs_(Patch, [], Unsorted),
-  sort(Unsorted, Graphs).
+commit_meta(Reactor, Graphs, Extra) :- commit_meta(Reactor, Graphs, [], Extra).
 
-collect_graphs_([], Acc, Graphs) :- Acc = Graphs.
-collect_graphs_([quint(G, _, _, _, _)|Patch], Acc, Graphs) :-
-  collect_graphs_(Patch, [G|Acc], Graphs).
+commit_meta(_, [], EAcc, Extra) :- append(EAcc, Extra). % flatten
+commit_meta(Reactor, [G|Graphs], EAcc, Extra) :-
+  meta_iri(G, Reactor, Meta),
+  data_iri(G, Reactor, Data),
+  declare_commit(G, Meta, Data, E),
+  commit_meta(Reactor, Graphs, [E|EAcc], Extra).
 
-commit_meta(Reactor, Graphs, Assertions, Retractions) :-
-  commit_meta_(Reactor, Graphs, [], Assertions, [], Retractions).
+declare_commit(Graph, Meta, Data, Extra) :-
+  (head(Graph, Head) ->
+    (Parent = [
+      quint(Meta, Meta, parent, Head, true),
+      % retract old head ref
+      quint(heads, Graph, head, Head, false)
+    ])
+  ; (Parent = [])),
 
-commit_meta_(_, [], AAcc, AOut, RAcc, ROut) :-
-  AAcc = AOut,
-  RAcc = ROut.
-commit_meta_(Reactor, [G|Graphs], AAcc, AOut, RAcc, ROut) :-
-  declare_commit(Reactor, G, CommitA),
-  declare_parent(Reactor, G, ParentA),
-  declare_ref(Reactor, G, RefA, RefR),
-  append([AAcc, CommitA, ParentA, RefA], ANext),
-  append([RAcc, RefR], RNext),
-  commit_meta_(Reactor, Graphs, ANext, AOut, RNext, ROut).
-
-declare_commit(Reactor, Graph, Assertions) :-
-  meta_iri(Graph, Reactor, Meta),
-  data_iri(Graph, Reactor, Data),
-  Assertions = [
+  Extra = [
+    quint(heads, Graph, head, Meta, true),
     quint(Meta, Meta, a, commit, true),
     quint(Meta, Meta, data, Data, true)
-  ].
-
-declare_parent(Reactor, Graph, Assertions) :-
-  head(Graph, Head) -> (
-    meta_iri(Graph, Reactor, Meta),
-    Assertions = [quint(Meta, Meta, parent, Head, true)]
-  ) ; Assertions = [].
-
-declare_ref(Reactor, Graph, Assertions, Retractions) :-
-  meta_iri(Graph, Reactor, Meta),
-  Assertions = [quint(heads, Graph, head, Meta, true)],
-  ( head(Graph, Head) ->
-      Retractions = [quint(heads, Graph, head, Head, true)]
-    ; Retractions = [] ).
+  | Parent ].
 
 % TODO: omit inapplicable quints? retractions of currently unasserted data, m.m.
 do_commit(Reactor, Patch) :-
-  full_commit(Reactor, Patch, Assertions, Retractions),
+  full_commit(Reactor, Patch, Processed, Extra),
   % why is this cut needed? probably because of the side effects after it:
   % prolog does the writes, then backtracks to full_commit, which now gives a
   % different result. but i don't really know.
   !,
-  maplist(retract, Retractions),
-  maplist(assertz, Assertions).
+  maplist(assertz, Processed),
+  maplist(do_write, Extra).
 
-full_commit(Reactor, Patch, Assertions, Retractions) :-
-  rewrite_graph_terms(Reactor, Patch, Rewritten),
-  collect_graphs(Patch, Graphs),
-  commit_meta(Reactor, Graphs, MetaAssertions, Retractions),
-  append([Rewritten, MetaAssertions], Assertions).
+do_write(quint(G, S, P, O, true)) :- assertz(quint(G, S, P, O, true)).
+do_write(quint(G, S, P, O, false)) :- retract(quint(G, S, P, O, true)).
 
-rewrite_graph_terms(Reactor, Patch, Rewritten) :-
-  rewrite_graph_terms_(Reactor, Patch, [], Rewritten).
+full_commit(Reactor, Patch, Processed, Extra) :-
+  process_patch_data(Reactor, Patch, Processed, Graphs),
+  % !, for optimization reasons? since we know the predicate is deterministic
+  commit_meta(Reactor, Graphs, Extra).
 
-rewrite_graph_terms_(_, [], Acc, Rewritten) :- Acc = Rewritten.
-rewrite_graph_terms_(Reactor, [quint(G, S, P, O, V)|Patch], Acc, Rewritten) :-
+process_patch_data(Reactor, Patch, Processed, Graphs) :-
+  process_patch_data(Reactor, Patch, [], Processed, [], Graphs).
+
+process_patch_data(_, [], PAcc, Processed, GAcc, Graphs) :-
+  PAcc = Processed,
+  sort(GAcc, Graphs).
+process_patch_data(Reactor, [quint(G, S, P, O, V)|Patch], PAcc, Processed, GAcc, Graphs) :-
   data_iri(G, Reactor, Data),
-  RAcc = [quint(Data, S, P, O, V)|Acc],
-  rewrite_graph_terms_(Reactor, Patch, RAcc, Rewritten).
+  process_patch_data(Reactor, Patch, [quint(Data, S, P, O, V)|PAcc], Processed, [G|GAcc], Graphs).
